d8758c6bd9fb2d66f4ad4ae06e1d44362b1a8be4
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2021-04-21 15:42:25 +0300

    Initial commit

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        ToDoRepository repository = new ToDoRepository();
+
+        // GET: api/<ToDoController>
+        [HttpGet]
+        public List<ToDoDto> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // POST api/<ToDoController>
+        [HttpPost]
+        public int Post([FromBody] ToDoDto toDoDto)
+        {
+            return repository.Create(toDoDto);
+        }
+
+        // PUT api/<ToDoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ToDoDto toDoDto)
+        {
+            repository.Update(id, toDoDto);
+        }
+    }
+}
---
+++ ToDoRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    public class ToDoRepository
+    {
+        private static List<ToDo> DataBase = new List<ToDo>();
+
+        private class ToDo
+        {
+            public int Id { get; }
+            public string Name { get; set; }
+            public bool Done { get; set; }
+
+            public DateTime creationDate { get; }
+
+            public ToDo(int id, string name, bool done)
+            {
+                Id = id;
+                Name = name;
+                Done = done;
+                creationDate = DateTime.Now;
+            }
+        }
+
+        private int GetId() => DataBase.Count > 0 ? DataBase.Max(x => x.Id) + 1 : 1;
+
+        public List<ToDoDto> GetAll()
+        {
+            return DataBase.ConvertAll(x => new ToDoDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Done = x.Done,
+            });
+        }
+
+        public int Create(ToDoDto toDoDto)
+        {
+            int id = GetId();
+            ToDo todo = new ToDo(id, toDoDto.Name, false);
+            DataBase.Add(todo);
+
+            return id;
+        }
+
+        public void Update(int id, ToDoDto toDoDto)
+        {
+            ToDo finded = DataBase.FirstOrDefault(x => x.Id == id);
+            if (finded == null) return;
+            finded.Name = toDoDto.Name;
+            finded.Done = toDoDto.Done;
+        }
+    }
+}
